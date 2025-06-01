8d70d6393bfc4ce0faa49f03bffcc4ace42c04a0
NefariousTechSupport <64808609+NefariousTechSupport@users.noreply.github.com>
2025-06-01 13:41:37 +0100

    Add build script to simplify things

---
+++ Wii/build.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+
+
+echo "Compiling dolinject..."
+cd $SCRIPT_DIR/tools/dolinject
+
+mkdir build
+cd build
+
+cmake -S .. -B . -G Ninja
+ninja
+
+
+
+echo "Generating makefile..."
+cd $SCRIPT_DIR
+$SCRIPT_DIR/tools/dolinject/build/dolinject compute-bases $1 Makefile-bases.autogen
+
+
+
+echo "Making..."
+make -f Makefile-bases.autogen clean
+make -f Makefile-bases.autogen SHELL="sh -x"
+
+
+echo "Injecting..."
+$SCRIPT_DIR/tools/dolinject/build/dolinject inject $1 $2 $3
